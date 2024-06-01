0b0be3a46338aac1694b0f2315df694f542ce3f7
Max Fonda <mxwll155@gmail.com>
2024-06-01 14:57:10 -0400

    requried login for all routes

--- app/api/channels_routes.py
+++ app/api/channels_routes.py
@@ -1,31 +1,36 @@
 from flask import Blueprint, request
 from app.models import db
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 
 channels_routes = Blueprint("channels", __name__)
 
 
 @channels_routes.route("/<int:id>")
+@login_required
 def get_channel(id):
     pass
 
 
 @channels_routes.route("/", methods=["POST"])
+@login_required
 def create_channel():
     pass
 
 
 @channels_routes.route("/<int:id>")
+@login_required
 def delete_channel(id):
     pass
 
 
 @channels_routes.route("/<int:id>/messages")
+@login_required
 def get_all_messages(id):
     pass
 
 
 @channels_routes.route("/<int:id>/messages", methods=["POST"])
+@login_required
 def create_message(id):
     pass
