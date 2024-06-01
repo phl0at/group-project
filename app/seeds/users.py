5ec835f56404554f5200deca1f3e9d5edc4cddd5
Max Fonda <mxwll155@gmail.com>
2024-06-01 12:58:52 -0400

    working on seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,4 +1,4 @@
-from app.models import db, User, environment, SCHEMA
+from app.models import db, User, Server, environment, SCHEMA
 from sqlalchemy.sql import text
 
 
@@ -28,5 +28,33 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
+    db.session.commit()
+
+
+
+# Adds a demo server, you can add other servers here if you want
+def seed_servers():
+
+
+    test_server = Server(
+            name='Test', DM=False, owner_id=1
+    )
+
+    db.session.add(test_server)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_servers():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.servers RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM servers"))
+
     db.session.commit()
