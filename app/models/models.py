d9b44b2e8ba724dd142d38c59a407ce9f815654c
Max Fonda <mxwll155@gmail.com>
2024-06-01 12:43:10 -0400

    models and migrations set up

--- app/models/models.py
+++ app/models/models.py
@@ -14,6 +14,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    servers = db.relationship("Server", backref="user")
+    messages = db.relationship("Message", backref="user")
+    reactions = db.relationship("Reaction", backref="user")
+
     @property
     def password(self):
         return self.hashed_password
@@ -31,3 +35,58 @@ class User(db.Model, UserMixin):
             'username': self.username,
             'email': self.email
         }
+
+
+class Server(db.Model):
+
+    __tablename__ = "servers"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    DM = db.Column(db.Boolean, nullable=False, default=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+
+
+class Channel(db.Model):
+
+    __tablename__ = "channels"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    messages = db.relationship("Message", backref='channel')
+
+
+class Message(db.Model):
+
+    __tablename__ = "messages"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    channel_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('channels.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    text = db.Column(db.String(250), nullable=False)
+
+    reactions = db.relationship("Reaction", backref='message')
+
+
+class Reaction(db.Model):
+
+    __tablename__ = 'reactions'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    message_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('messages.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
