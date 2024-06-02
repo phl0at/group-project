25713f8178ba11197ec8d0e1f2b7ca39fa88c497
dstudenko <dstudenko@gmail.com>
2024-06-02 10:48:16 -0400

    images polymorphic association

--- app/models/models.py
+++ app/models/models.py
@@ -5,44 +5,6 @@ from flask_login import UserMixin
 from sqlalchemy.orm import validates
 
 
-class Image(db.Model):
-    __tablename__ = 'images'
-
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50), nullable=False)
-    type_id = db.Column(db.Integer, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-
-
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=True)
-    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('servers.id')), nullable=True)
-    message_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('messages.id')), nullable=True)
-
-
-    # @validates("type")
-    # def validate_type(self, key, value):
-    #     valid_tables = ['user', 'server', 'message']
-    #     if value not in valid_tables:
-    #         raise ValueError("Invalid type")
-    #     return value
-
-    __mapper_args__ = {
-        "polymorphic_on": type,
-        "polymorphic_identity": "image"
-    }
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'type': self.type,
-            'type_id': self.type_id,
-            'img_url': self.img_url
-        }
-
-
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
@@ -54,9 +16,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
-    servers = db.relationship("Server", backref="user", cascade='all, delete-orphan')
-    messages = db.relationship("Message", backref="user", cascade='all, delete-orphan')
-    images = db.relationship("Image", primaryjoin="and_(Image.user_id==User.id, Image.type=='user')", backref="user")
+    images = db.relationship('Image', backref='user', primaryjoin="and_(Image.type=='user', foreign(Image.type_id)==User.id)", lazy=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "user"
@@ -94,7 +54,8 @@ class Server(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     DM = db.Column(db.Boolean, nullable=False, default=False)
     owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    images = db.relationship("Image", primaryjoin="and_(Image.server_id==Server.id, Image.type=='server')", backref="server")
+
+    images = db.relationship('Image', backref='server', primaryjoin="and_(Image.type=='server', foreign(Image.type_id)==Server.id)", lazy=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "server"
@@ -142,7 +103,8 @@ class Message(db.Model):
     channel_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('channels.id')), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     text = db.Column(db.String(250), nullable=False)
-    images = db.relationship("Image", primaryjoin="and_(Image.message_id==Message.id, Image.type=='message')", backref="message")
+
+    images = db.relationship('Image', backref='message', primaryjoin="and_(Image.type=='message', foreign(Image.type_id)==Message.id)", lazy=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "message"
@@ -176,4 +138,38 @@ class Reaction(db.Model):
             'message': self.message_id,
             'user': self.user_id,
             'type': self.type
+        }
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    type_id = db.Column(db.Integer, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+
+    # @validates("type")
+    # def validate_type(self, key, value):
+    #     valid_tables = ['user', 'server', 'message']
+    #     if value not in valid_tables:
+    #         raise ValueError("Invalid type")
+    #     return value
+
+    __mapper_args__ = {
+        "polymorphic_on": type,
+        "polymorphic_identity": "image"
+    }
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'type': self.type,
+            'type_id': self.type_id,
+            'img_url': self.img_url
         }
