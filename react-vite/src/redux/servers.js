80ef21237803bf1caf2232f086b2b5d602a231e1
Max Fonda <mxwll155@gmail.com>
2024-06-04 00:12:43 -0400

    added reselect to pkg dependencies

--- react-vite/src/redux/servers.js
+++ react-vite/src/redux/servers.js
@@ -1,4 +1,5 @@
 import { csrfFetch } from "./csrf";
+import { createSelector } from "reselect";
 
 //! --------------------------------------------------------------------
 //*                          Action Types
