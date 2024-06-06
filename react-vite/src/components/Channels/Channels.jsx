385819cdfc12257330e7cdecf32d497fdf7beaed
dstudenko <dstudenko@gmail.com>
2024-06-05 20:19:55 -0400

    create channel functionality complete

--- react-vite/src/components/Channels/Channels.jsx
+++ react-vite/src/components/Channels/Channels.jsx
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllMessagesThunk } from "../../redux/messages";
 import OpenModalMenuItem from "../Main/OpenModalMenuItem";
 import EditChannelModel from "../EditChannelModal ";
+import CreateChannelModal from "../Channels/CreateChannelModal"
 import { CiEdit } from "react-icons/ci";
 
 function ChannelsList() {
   const dispatch = useDispatch();
   const channels = useSelector(getChannelsArray);
   const server = useSelector((state) => state.server.current);
+
 
   if (!server) return "";
   if (!channels.length) return "No channels in this server!";
@@ -20,27 +22,40 @@ function ChannelsList() {
 
   return (
     <>
-      {server &&
-        channels.map((channel) => {
-          if (channel.server_id === server.id) {
-            return (
-              <div key={channel.id}>
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleChannelClick(channel);
-                  }}
-                >
-                  {channel.name}
-                </button>
-                <OpenModalMenuItem
-                  itemText={<CiEdit />}
-                  modalComponent={<EditChannelModel channel={channel} />}
-                />
-              </div>
-            );
+      <div>
+        <OpenModalMenuItem
+          itemText={
+            <>
+              Create Channel: <CiEdit />
+            </>
           }
-        })}
+          modalComponent={<CreateChannelModal serverId={server.id} />}
+        />
+      </div>
+      <div>
+        {server &&
+          channels.map((channel) => {
+            if (channel.server_id === server.id) {
+              return (
+                <div key={channel.id}>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleChannelClick(channel);
+                    }}
+                  >
+                    {channel.name}
+                  </button>
+                  <OpenModalMenuItem
+                    itemText={<CiEdit />}
+                    modalComponent={<EditChannelModel channel={channel} />}
+                  />
+                </div>
+              );
+            }
+          })}
+      </div>
+
     </>
   );
 }
--- react-vite/src/components/Channels/CreateChannelModal/CreateChannelModal.jsx
+++ react-vite/src/components/Channels/CreateChannelModal/CreateChannelModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import { createChannelThunk } from "../../redux/channels";
+import { useModal } from "../../../context/Modal";
+import { createChannelThunk } from "../../../redux/channels";
 
 const CreateChannelModal = ({ serverId }) => {
     const dispatch = useDispatch();
