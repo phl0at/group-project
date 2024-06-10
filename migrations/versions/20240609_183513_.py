4f6a11d20c68a18c27bcd8b7fe747698e649e9e1
dstudenko <dstudenko@gmail.com>
2024-06-10 19:59:10 -0400

    edit migrations

--- migrations/versions/20240609_183513_.py
+++ migrations/versions/20240609_183513_.py
@@ -7,6 +7,9 @@ Create Date: 2024-06-09 18:35:13.842802
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv('FLASK_ENV')
+SCHEMA = os.environ.get('SCHEMA')
 
 
 # revision identifiers, used by Alembic.
@@ -65,6 +68,12 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment=='production':
+        op.execute(f'ALTER TABLE users SET SCHEMA {SCHEMA};')
+        op.execute(f'ALTER TABLE channels SET SCHEMA {SCHEMA};')
+        op.execute(f'ALTER TABLE reactions SET SCHEMA {SCHEMA};')
+        op.execute(f'ALTER TABLE servers SET SCHEMA {SCHEMA};')
+        op.execute(f'ALTER TABLE messages SET SCHEMA {SCHEMA};')
     # ### end Alembic commands ###
 
 
