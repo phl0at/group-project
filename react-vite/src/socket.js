387753cd41b9d774f641e3fa947dc0df8ac4b326
dstudenko <dstudenko@gmail.com>
2024-06-13 18:59:24 -0400

    add hypercomm url

--- react-vite/src/socket.js
+++ react-vite/src/socket.js
@@ -4,5 +4,6 @@ export const socket = io(
   // import.meta.env.NODE_ENV === "production"
   //   ? "ws://hypercomm.onrender.com"
   //   : "http://localhost:8000", {autoConnect: false}
-  import.meta.env.SOCKET_URL
+  // import.meta.env.SOCKET_URL
+  'ws://hypercomm.onrender.com/'
 );
