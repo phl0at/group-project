966c6c5898d9f3fe5c7e407fac339e35bf947551
dstudenko <dstudenko@gmail.com>
2024-06-13 18:48:15 -0400

    add SOCKET URL for render

--- react-vite/src/socket.js
+++ react-vite/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.NODE_ENV === "production"
-    ? "ws://hypercomm.onrender.com"
-    : "http://localhost:8000", {autoConnect: false}
+  // import.meta.env.NODE_ENV === "production"
+  //   ? "ws://hypercomm.onrender.com"
+  //   : "http://localhost:8000", {autoConnect: false}
+  import.meta.env.SOCKET_URL
 );
