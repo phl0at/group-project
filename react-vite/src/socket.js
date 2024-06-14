90300845a69701b9bbdb89a6612947740784fe07
Max Fonda <mxwll155@gmail.com>
2024-06-14 17:37:17 -0400

    update prod url

--- react-vite/src/socket.js
+++ react-vite/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.PROD
-  ? "hypercomm.onrender.com"
+  ? "wss://hypercomm.onrender.com"
   : "http://localhost:8080";
 
 export const socket = io(SOCKET_URL, {
